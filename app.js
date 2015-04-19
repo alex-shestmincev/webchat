fac7f9e6d3d513a626c0145ca8b80ee3dc273fae
Alex <shestmintsev@gmail.com>
2015-04-19 22:20:32 +0300

    templates

--- app.js
+++ app.js
@@ -1,41 +1,34 @@
 var express = require('express');
 var http = require('http');
+var path = require('path');
+var config = require('config');
+var log = require('libs/log')(module);
+var favicon = require('serve-favicon');
+var logger = require('morgan');
 
 var app = express();
-app.set('port',3000);
+app.engine('ejs', require('ejs-locals'));
+app.set('views', path.join(__dirname, 'templates'));
+app.set('view engine', 'ejs');
 
-http.createServer(app).listen(app.get('port'),function(){
-  console.log('Express server listening on port 3000');
+app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+if(app.get('env') == 'development'){
+  app.use(express.logger('dev'));
+} else {
+  app.use(express.logger('default'));
+}
+
+app.use(express.bodyParser()); // req.body
+app.use(express.cookieParser()); // req.cookies
+
+app.use(app.router);
+
+app.get('/',function(req, res, end){
+  res.render("index");
 });
 
-// Middleware
-app.use(function(req,res, next){
-  if (req.url == '/'){
-    res.end("Hello");
-  }else{
-    next();
-  }
-});
-
-app.use(function(req,res, next){
-  if (req.url == '/forbidden'){
-    next(new Error("wops, denied"));
-  }else{
-    next();
-  }
-});
-
-app.use(function(req,res, next){
-  if (req.url == '/test'){
-    res.end("Test");
-  }else{
-    next();
-  }
-});
-
-app.use(function(req,res){
-  res.send(404, "Page Not Found Sorry");
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(err, req, res, next){
   // NODE_ENV = 'production'
@@ -47,60 +40,6 @@ app.use(function(err, req, res, next){
   }
 });
 
-//var path = require('path');
-//var favicon = require('static-favicon');
-//var logger = require('morgan');
-//var cookieParser = require('cookie-parser');
-//var bodyParser = require('body-parser');
-//
-//var routes = require('./routes');
-//var users = require('./routes/user');
-//
-//var app = express();
-//
-//// view engine setup
-//app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'ejs');
-//
-//app.use(favicon());
-//app.use(logger('dev'));
-//app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded());
-//app.use(cookieParser());
-//app.use(express.static(path.join(__dirname, 'public')));
-//app.use(app.router);
-//
-//app.get('/', routes.index);
-//app.get('/users', users.list);
-//
-///// catch 404 and forwarding to error handler
-//app.use(function(req, res, next) {
-//    var err = new Error('Not Found');
-//    err.status = 404;
-//    next(err);
-//});
-//
-///// error handlers
-//
-//// development error handler
-//// will print stacktrace
-//if (app.get('env') === 'development') {
-//    app.use(function(err, req, res, next) {
-//        res.render('error', {
-//            message: err.message,
-//            error: err
-//        });
-//    });
-//}
-//
-//// production error handler
-//// no stacktraces leaked to user
-//app.use(function(err, req, res, next) {
-//    res.render('error', {
-//        message: err.message,
-//        error: {}
-//    });
-//});
-//
-//
-//module.exports = app;
+http.createServer(app).listen(config.get('port'),function(){
+  log.info('Express server listening on port ' + config.get('port'));
+});
